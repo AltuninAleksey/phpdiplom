6bc979a6891d7f79b8073e63067efc9e33dbdb6f
AltuninAleksey <altunin.lescha@gmail.com>
2022-05-18 16:19:59 +0300

    18/05/2022

--- repair.php
+++ repair.php
@@ -72,37 +72,37 @@
                             <div class="products">
 
                                 <div class="product">
-                                    <img src="img/kvadro.jpg" alt="">
+                                    <img src="img/bike.jpg" alt="">
                                     <div class="text">Диагностика</div>
                                     <button  onclick="location.href='diagnostik.php';"> ПОДРОБНЕЕ</button>
                                 </div>
 
                                 <div class="product">
-                                    <img src="img/kvadro.jpg" alt="">
+                                    <img src="img/suspersion.jpg" alt="">
                                     <div class="text">Подвеска</div>
                                     <button onclick="location.href='suspersion.php';">ПОДРОБНЕЕ</button>
                                 </div>
 
                                 <div class="product">
-                                    <img src="img/kvadro.jpg" alt="">
+                                    <img src="img/collapse.jpg" alt="">
                                     <div class="text">Сход развал</div>
                                     <button onclick="location.href='collpase.php';">ПОДРОБНЕЕ</button>
                                 </div>
 
                                 <div class="product">
-                                    <img src="img/kvadro.jpg" alt="">
+                                    <img src="img/card.png" alt="">
                                     <div class="text">замена масла</div>
                                     <button onclick="location.href='oil.php';">ПОДРОБНЕЕ</button>
                                 </div>
 
                                 <div class="product">
-                                    <img src="img/kvadro.jpg" alt="">
+                                    <img src="img/tire.jpg" alt="">
                                     <div class="text">Шиномонтаж</div>
                                     <button onclick="location.href='tire.php';">ПОДРОБНЕЕ</button>
                                 </div>
 
                                 <div class="product">
-                                    <img src="img/kvadro.jpg" alt="">
+                                    <img src="img/trow.jpg" alt="">
                                     <div class="text">Эвакуатор</div>
                                     <button onclick="location.href='truck.php';">ПОДРОБНЕЕ</button>
                                 </div>
